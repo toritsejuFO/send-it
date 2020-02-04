1c4acc7e6c0af0c57e5ae4c0e812239af163a3cd
Faith Odonghanro <toriboi.fo@gmail.com>
2020-02-04 17:14:26 +0100

    Write tests for login feature

--- test/auth.js
+++ test/auth.js
@@ -22,68 +22,119 @@ describe('Auth API', () => {
     db.close();
   });
 
-  it('should signup a new user successfully', (done) => {
-    chai.request(server)
-      .post('/api/v1/auth/signup')
-      .send({
-        firstname: 'Jane',
-        lastname: 'Doe',
-        othernames: 'Alice',
-        email: 'janedoe@gmail.com',
-        username: 'alice',
-        password: 'smallCAPSNumb3rs&$ymb0ls',
-        isAdmin: false,
-        phone: '09012345678',
-      })
-      .end((err, res) => {
-        expect(err).to.be.null;
-        expect(res).to.be.an('object');
-        expect(res.body.status).to.equal(201);
-        expect(res.body.data[0].user.email).to.equal('janedoe@gmail.com');
-        expect(res.body.data[0]).to.haveOwnProperty('token');
-        done();
-      });
+  describe('Signup', () => {
+    it('should signup a new user successfully', (done) => {
+      chai.request(server)
+        .post('/api/v1/auth/signup')
+        .send({
+          firstname: 'Jane',
+          lastname: 'Doe',
+          othernames: 'Alice',
+          email: 'janedoe@gmail.com',
+          username: 'alice',
+          password: 'smallCAPSNumb3rs&$ymb0ls',
+          isAdmin: false,
+          phone: '09012345678',
+        })
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.be.an('object');
+          expect(res.body.status).to.equal(201);
+          expect(res.body.data[0].user.email).to.equal('janedoe@gmail.com');
+          expect(res.body.data[0]).to.haveOwnProperty('token');
+          done();
+        });
+    });
+
+    it('should not signup a new user with email that already exists', (done) => {
+      chai.request(server)
+        .post('/api/v1/auth/signup')
+        .send({
+          firstname: 'John',
+          lastname: 'Doe',
+          othernames: 'Bob',
+          email: 'janedoe@gmail.com',
+          username: 'bob',
+          password: 'smallCAPSNumb3rs&$ymb0ls',
+          isAdmin: false,
+          phone: '09012345678',
+        })
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.be.an('object');
+          expect(res.status).to.equal(409);
+          done();
+        });
+    });
+
+    it('should not signup a new user with username that already exists', (done) => {
+      chai.request(server)
+        .post('/api/v1/auth/signup')
+        .send({
+          firstname: 'John',
+          lastname: 'Doe',
+          othernames: 'Bob',
+          email: 'johndoe@gmail.com',
+          username: 'alice',
+          password: 'smallCAPSNumb3rs&$ymb0ls',
+          isAdmin: false,
+          phone: '09012345678',
+        })
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.be.an('object');
+          expect(res.status).to.equal(409);
+          done();
+        });
+    });
   });
 
-  it('should not signup a new user with email that already exists', (done) => {
-    chai.request(server)
-      .post('/api/v1/auth/signup')
-      .send({
-        firstname: 'John',
-        lastname: 'Doe',
-        othernames: 'Bob',
-        email: 'janedoe@gmail.com',
-        username: 'bob',
-        password: 'smallCAPSNumb3rs&$ymb0ls',
-        isAdmin: false,
-        phone: '09012345678',
-      })
-      .end((err, res) => {
-        expect(err).to.be.null;
-        expect(res).to.be.an('object');
-        expect(res.status).to.equal(409);
-        done();
-      });
-  });
+  describe('Login', () => {
+    it('should login user successfully', (done) => {
+      chai.request(server)
+        .post('/api/v1/auth/login')
+        .send({
+          userId: 'janedoe@gmail.com',
+          password: 'smallCAPSNumb3rs&$ymb0ls',
+        })
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.be.an('object');
+          expect(res.status).to.equal(200);
+          expect(res.body.data[0].user.email).to.equal('janedoe@gmail.com');
+          expect(res.body.data[0]).to.haveOwnProperty('token');
+          done();
+        });
+    });
 
-  it('should not signup a new user with username that already exists', (done) => {
-    chai.request(server)
-      .post('/api/v1/auth/signup')
-      .send({
-        firstname: 'John',
-        lastname: 'Doe',
-        othernames: 'Bob',
-        email: 'johndoe@gmail.com',
-        username: 'alice',
-        password: 'smallCAPSNumb3rs&$ymb0ls',
-        isAdmin: false,
-        phone: '09012345678',
-      })
-      .end((err, res) => {
-        expect(err).to.be.null;
-        expect(res).to.be.an('object');
-        expect(res.status).to.equal(409);
-        done();
-      });
+    it('should not login non-existing user with incorrect email/username', (done) => {
+      chai.request(server)
+        .post('/api/v1/auth/login')
+        .send({
+          userId: 'johndoe@gmail.com',
+          password: 'smallCAPSNumb3rs&$ymb0ls',
+        })
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.be.an('object');
+          expect(res.status).to.equal(401);
+          done();
+        });
+    });
+
+    it('should not login existing user with incorrect password', (done) => {
+      chai.request(server)
+        .post('/api/v1/auth/login')
+        .send({
+          userId: 'janedoe@gmail.com',
+          password: 'f@k3P@ssW0rd',
+        })
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.be.an('object');
+          expect(res.status).to.equal(401);
+          done();
+        });
+    });
   });
 });
