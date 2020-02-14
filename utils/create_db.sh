f6e21becab1266be7bc8b6f85a4499b04e2a2634
Faith Odonghanro <toriboi.fo@gmail.com>
2020-02-14 17:25:00 +0100

    Update command to create parcel table

--- utils/create_db.sh
+++ utils/create_db.sh
@@ -11,6 +11,6 @@ psql \
   -c "\c $DB_NAME;" \
   -c "CREATE TABLE users (id SERIAL UNIQUE NOT NULL, firstname VARCHAR(50) NOT NULL, lastname VARCHAR(50) NOT NULL, othernames VARCHAR(50), email VARCHAR(128) UNIQUE NOT NULL, username VARCHAR(50) UNIQUE NOT NULL, password VARCHAR(60) NOT NULL, registered DATE DEFAULT NOW(), isAdmin BOOL, phone VARCHAR(15) NOT NULL, PRIMARY KEY(id));" \
   -c "CREATE TYPE sendit_status_enum AS ENUM ('placed', 'transiting', 'delivered');" \
-  -c "CREATE TABLE parcels (id serial UNIQUE NOT NULL, placedBy INTEGER NOT NULL REFERENCES users(id), weight FLOAT, weightmetrics VARCHAR(10), deliveredOn DATE NOT NULL, status sendit_status_enum NOT NULL, fromAddress VARCHAR(100), toAddress VARCHAR(100), currentLocation VARCHAR(100), PRIMARY KEY(id));" \
+  -c "CREATE TABLE parcels (id serial UNIQUE NOT NULL, placedBy INTEGER NOT NULL REFERENCES users(id), weight FLOAT, weightmetric VARCHAR(10), sentOn TIMESTAMP default NOW(), deliveredOn TIMESTAMP, status sendit_status_enum NOT NULL, fromAddress VARCHAR(100), toAddress VARCHAR(100), currentLocation VARCHAR(100), PRIMARY KEY(id));" \
   -U postgres \
   -h localhost
