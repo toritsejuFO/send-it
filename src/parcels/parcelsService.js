e22132358c6495bf03d1ac9f5f6d27763b700000
Faith Odonghanro <toriboi.fo@gmail.com>
2020-02-19 00:22:35 +0100

    Update

--- src/parcels/parcelsService.js
+++ src/parcels/parcelsService.js
@@ -7,16 +7,20 @@ export default class ParcelsService {
   static createDeliveryOrder = async (newParcelOrder) => {
     const parcel = new Parcel();
     await parcel.create(newParcelOrder);
+
     if (parcel.hasError()) {
       errorLogger.error(parcel.getErrorStack(), { file: __filename });
       throw new Error('Unable to create parcel delivery order');
     }
+
     return parcel.getDetails()[0];
   }
+
 
   static findById = async (parcelId) => {
     const parcel = new Parcel();
     await parcel.findById(parcelId);
+
     if (parcel.hasError()) {
       errorLogger.error(parcel.getErrorStack(), { file: __filename });
       throw new Error('Unable to find parcel');
